5a46ad508f826a972b7f89f9d452745cafdcab41
vishnuishpujani <vishnuishpujani.vi@gmail.com>
2020-10-14 22:38:31 +0530

    test cases changes

--- src/test/java/com/mycompany/app/NewTest.java
+++ src/test/java/com/mycompany/app/NewTest.java
@@ -1,6 +1,11 @@
 package com.mycompany.app;
 
-import org.openqa.selenium.By;		
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;		
 import org.testng.Assert;		
@@ -8,7 +13,8 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;	
 import org.testng.annotations.AfterTest;		
 public class NewTest {		
-	    private WebDriver driver;		
+	    private WebDriver driver;	
+	    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		@Test				
 		public void testEasy() {	
 			driver.get("http://demo.guru99.com/test/guru99home/");  
@@ -23,5 +29,36 @@ public class NewTest {
 		@AfterTest
 		public void afterTest() {
 			driver.quit();			
-		}		
+		}	
+		
+
+	    @BeforeTest
+	    public void setUpStreams() {
+	        System.setOut(new PrintStream(outContent));
+	    }
+
+	    @Test
+	    public void testAppConstructor() {
+	        try {
+	            new App();
+	        } catch (Exception e) {
+	            fail("Construction failed.");
+	        }
+	    }
+
+	    @Test
+	    public void testAppMain()
+	    {
+	        App.main(null);
+	        try {
+	            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+	        } catch (AssertionError e) {
+	            fail("\"message\" is not \"Hello World!\"");
+	        }
+	    }
+
+	    @AfterTest
+	    public void cleanUpStreams() {
+	        System.setOut(null);
+	    }
 }	
